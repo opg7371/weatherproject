12e5991b55d1303102dc06c55db7f60dd9cbae18
Piyush Badkul <guptatrue18@gmail.com>
2016-08-16 23:53:04 +0530

    Comments Fixed

--- app/src/main/java/myproject/weatherproject/WeatherHttpClient.java
+++ app/src/main/java/myproject/weatherproject/WeatherHttpClient.java
@@ -12,21 +12,7 @@ package myproject.weatherproject;
     import java.io.InputStreamReader;
     import java.net.HttpURLConnection;
     import java.net.URL;
-    /*
-     * Copyright (C) 2013 Surviving with Android (http://www.survivingwithandroid.com)
-     *
-     * Licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     *      http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
+   
     public class WeatherHttpClient {
 
         private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
